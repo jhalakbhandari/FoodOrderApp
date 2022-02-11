56e83080ca974cf1ee88868d061639fd09bff229
Jhalakbhandari <bhandari.jhalak2018@gmail.com>
2022-02-11 12:58:40 +0530

    initial commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+import { Fragment } from "react";
+const Meals = ()=>{
+    return <Fragment>
+        
+        <MealsSummary/>
+        <AvailableMeals/>
+    </Fragment>
+};
+export default Meals;
